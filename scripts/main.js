3d101d0b232693e18987af3ffa862a1205d39cab
GUICHON Thierry <thierry.guichon@etu.univ-lyon1.fr>
2022-11-14 08:46:16 +0100

    Base index.html, structure du projet.

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// jshint browser:true, eqeqeq:true, undef:true, devel:true, esversion: 6
+
+const cnvMap = document.createElementById("snakeMap");
+
+class clsSnakeMap {
+    
+    constructor(divWith, divHeight) {
+
+    }
+
+}
+
+
+function main() {
+    console.log("Ca marche");
+}
+
+main();
